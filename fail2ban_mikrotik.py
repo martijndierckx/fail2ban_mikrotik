5ee0b7f567af2f0f14e26e408426f2c5da4f186d
martijndierckx <martijn@smard.be>
2025-03-17 17:52:39 +0100

    Use RouterOS-api instead of tikapy

--- fail2ban_mikrotik.py
+++ fail2ban_mikrotik.py
@@ -1,60 +1,48 @@
 #!/usr/bin/python3
-#
-# Mikrotik Fail2ban script v0.2.1 #
-#				#
-#	         by Mauro Fiore	#
-#	blog.openskills.it	#
-#
-import argparse,sys
-from tikapy import TikapyClient
-from pprint import pprint
 
+import sys
+import argparse
+from routeros_api import RouterOsApiPool
 
-parser = argparse.ArgumentParser(description="Fail2Ban Mikrotik Script 0.2")
-parser.add_argument('-m', help='Mikrotik ip',required=True)
-parser.add_argument('-s', help='Mikrotik API port',required=True)
-parser.add_argument('-u', help='Mikrotik API User',required=True)
-parser.add_argument('-p', help='Mikrotik API Password',required=True)
-parser.add_argument('-a', help='Action: ban or unban',required=True)
-parser.add_argument('-i', help='Ip address',required=True)
-parser.add_argument('-l', help='address List', required=True)
-parser.add_argument('-d', help='dinamic timeout', required=False)
+parser = argparse.ArgumentParser(description="Fail2Ban Mikrotik Script")
+parser.add_argument('-m', help='Mikrotik IP', required=True)
+parser.add_argument('-s', help='Mikrotik API port', required=True, type=int)
+parser.add_argument('-u', help='Mikrotik API User', required=True)
+parser.add_argument('-p', help='Mikrotik API Password', required=True)
+parser.add_argument('-a', help='Action: ban or unban', required=True)
+parser.add_argument('-i', help='IP address', required=True)
+parser.add_argument('-l', help='Address List', required=True)
+parser.add_argument('-d', help='Dynamic timeout', required=False)
 args = parser.parse_args()
- 
-client = TikapyClient(args.m,int(args.s))
 
-client.login(args.u,args.p)
+try:
+    api_pool = RouterOsApiPool(args.m, username=args.u, password=args.p, port=args.s, plaintext_login=True)
+    api = api_pool.get_api()
+    address_list = api.get_resource('/ip/firewall/address-list')
 
-addresslist="?=list="+args.l
-ip="?=address="+args.i
-dic=client.talk(['/ip/firewall/address-list/print',ip,addresslist,])
+    existing_entries = address_list.get(address=args.i, list=args.l)
 
-if args.a == "ban":
+    if args.a == "ban":
+        if not existing_entries:
+            entry = {'list': args.l, 'address': args.i}
+            if args.d:
+                entry['timeout'] = args.d
+            address_list.add(**entry)
+            print(f"IP {args.i} added to list {args.l}")
+        else:
+            print(f"IP {args.i} in list {args.l} already exists")
+            sys.exit(1)
 
-	if not bool(dic):
-		addresslist="=list="+args.l
-		ip="=address="+args.i
-		if args.d:
-			dynamic="dynamic=yes"
-			timeout="=timeout="+args.d
-			client.talk(['/ip/firewall/address-list/add',addresslist,ip,dynamic,timeout])
-		else: 
-			client.talk(['/ip/firewall/address-list/add',addresslist,ip,])
-		sys.exit(0)
-	else:
-		print ("IP "+args.i+" in list "+args.l+" already exist")
-		sys.exit(1)
+    elif args.a == "unban":
+        if existing_entries:
+            for entry in existing_entries:
+                address_list.remove(id=entry['id'])
+            print(f"IP {args.i} removed from list {args.l}")
+        else:
+            print(f"IP {args.i} in list {args.l} does not exist")
+            sys.exit(1)
 
-elif args.a == "unban":
-
-	if bool(dic):
-		for key in dic:
-			nid=dic[key].get(".id")
-		remid="=.id="+nid	
-		client.talk(['/ip/firewall/address-list/remove',remid,])
-		sys.exit(0)
-	else:
-		print ("IP "+args.i+" in list "+args.l+" doesn't exist")
-		sys.exit(1)
-else:
-	sys.exit(1)
+    api_pool.disconnect()
+except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
